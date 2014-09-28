4511456985027d614a480629b91a557ec38888d1
John Kelly <mr.johnkelly@live.com>
2014-09-27 23:25:57 -0700

    Refactor Sig Figs Default to a constant

--- lib/calculator/base.rb
+++ lib/calculator/base.rb
@@ -1,5 +1,7 @@
 module Calculator
   class Base
+    DEFAULT_SIGNIFICANT_FIGURES = 15
+
     def add(*args)
       fail ArgumentError unless args.flatten.length > 1
       args.flatten.map(&:to_s).map { |value| BigDecimal.new(value) }.inject(:+)
@@ -42,21 +44,21 @@ module Calculator
       number.to_i == 0 ? 1 : multiply(*(1..number))
     end
 
-    def ln(number, decimal_places = 15)
+    def ln(number, decimal_places = DEFAULT_SIGNIFICANT_FIGURES)
       fail ArgumentError unless BigDecimal.new(number.to_s) > BigDecimal.new(0)
       BigMath::log(BigDecimal.new(number.to_s), decimal_places).truncate(decimal_places)
     end
 
-    def log(number, base = 10, decimal_places = 15)
+    def log(number, base = 10, decimal_places = DEFAULT_SIGNIFICANT_FIGURES)
       fail ArgumentError unless BigDecimal.new(number.to_s) > BigDecimal.new(0)
       divide(ln(BigDecimal.new(number.to_s), decimal_places), ln(BigDecimal.new(base.to_s), decimal_places))
     end
 
-    def E(decimal_places = 15)
+    def E(decimal_places = DEFAULT_SIGNIFICANT_FIGURES)
       BigMath::E(decimal_places).truncate(decimal_places)
     end
 
-    def PI(decimal_places = 15)
+    def PI(decimal_places = DEFAULT_SIGNIFICANT_FIGURES)
       BigMath::PI(decimal_places).truncate(decimal_places)
     end
   end
